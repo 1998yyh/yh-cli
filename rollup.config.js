a7a698a0258f488041ba1089f4c8b2ca477b95e6
hao.yang <hao.yang@haoyangdeMacBook-Pro.local>
2023-01-30 18:52:04 +0800

    提交

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import rollupTypescript from '@rollup/plugin-typescript'
-import rollupNodeResolve from '@rollup/plugin-node-resolve'
 import rollupBabel from '@rollup/plugin-babel'
 import rollupAlias from '@rollup/plugin-alias'
 import rollupJson from '@rollup/plugin-json'
 import rollupCommonjs from '@rollup/plugin-commonjs'
+import rollupCleaner from 'rollup-plugin-cleaner'
+import rollupNodeResolve from '@rollup/plugin-node-resolve'
+import hashbang from "rollup-plugin-hashbang";
 
 
 
 const plugins = [
+  rollupCleaner({
+    targets:['./dist']
+  }),
   rollupBabel({
     babelrc: false,
     babelHelpers: 'bundled',
@@ -24,11 +29,16 @@ const plugins = [
       '@typescript-eslint/ban-ts-comment': 2
     }
   }),
-  // rollupNodeResolve(),
+  rollupNodeResolve(),
   rollupAlias(),
+  // 处理json 转化为 esmodule
   rollupJson(),
+  // 编译typescript
   rollupTypescript(),
-  rollupCommonjs()
+  // esmodule 转化为 commonjs
+  rollupCommonjs(),
+  // 处理文件顶部 #!/usr/bin/env node
+  hashbang.default()
 ]
 
 
@@ -36,15 +46,15 @@ const buildOptions = {
   input:['src/index.ts'],
   output:[
     {
-      dir:'dist/es',
+    
+      file: 'dist/index.mjs',
       format:'esm'
     },
     {
-      dir:'dist/cjs',
+      file:'dist/index.cjs',
       format:'cjs'
     }
   ],
-  external:[],
   plugins
 }
 
