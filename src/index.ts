4da90f0876968e21ed866a24093e4972f873a514
hao.yang <hao.yang@haoyangdeMacBook-Pro.local>
2023-01-31 16:16:18 +0800

    创建cli

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,82 @@
 #!/usr/bin/env node
 const { Command } = require('commander')
-const packageJson  = require('../package.json')
 const program = new Command()
 const inquirer = require('inquirer')
+const chalk = require('chalk')
+const spawn = require('cross-spawn')
+
+// 配置版本号信息
+program
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
 
 
-program.version(packageJson.version, '-v, --version', '@calmer/terminal-cli 当前版本')
+// 创建项目
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .action((name:unknown, options:unknown) => {
+    // 打印执行结果
+    console.log('name:',name,'options:',options)
+  })
+  
 
-program.on('help',()=>{
-  console.log('help')
+// 这块是去测试
+program
+  .command('install')
+  .description('print debugging information about your environment')
+  .action(() => {
+    // 定义需要按照的依赖
+    const dependencies = ['vue', 'vuex', 'vue-router'];
+    // 执行安装
+    const child = spawn('npm', ['install', '-D'].concat(dependencies), {
+      stdio: 'inherit'
+    });
+
+    // 监听执行结果
+    child.on('close', function (code: number) {
+      // 执行失败
+      if (code !== 0) {
+        console.log(chalk.red('Error occurred while installing dependencies!'));
+        process.exit(1);
+      }
+      // 执行成功
+      else {
+        console.log(chalk.cyan('Install finished'))
+      }
+    })
+  })
+
+// 本地环境信息
+program
+  .command('info')
+  .description('print debugging information about your environment')
+  .action(() => {
+    require('envinfo').run(
+      {
+        System: ['OS', 'CPU'],
+        Binaries: ['Node', 'Yarn', 'npm'],
+        Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
+        npmPackages: '/**/{typescript,*vue*,@vue/*/}',
+        npmGlobalPackages: ['@vue/cli']
+      },
+      {
+        showNotFound: true,
+        duplicates: true,
+        fullTree: true
+      }
+    ).then(console.log)
+  })
+
+program.on('command:*', () => {
+  console.log('command')
+})
+
+
+program.on('--help', () => {
+  console.log();
+  console.log(`  Run ${chalk.cyan(`vue <command> --help`)} for detailed usage of given command.`)
+  console.log()
 })
 
 // 解析用户执行命令传入的参数
