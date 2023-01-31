628128e0b29b4a5fff051cc05a514290342044f6
hao.yang <hao.yang@haoyangdeMacBook-Pro.local>
2023-01-31 15:43:47 +0800

    修改组件版本

--- rollup.config.js
+++ rollup.config.js
@@ -46,11 +46,6 @@ const buildOptions = {
   input:['src/index.ts'],
   output:[
     {
-    
-      file: 'dist/index.mjs',
-      format:'esm'
-    },
-    {
       file:'dist/index.cjs',
       format:'cjs'
     }
